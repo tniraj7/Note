7e5e09b01675b46d06a3cc3f003329b74e51f397
Niraj Tiwari <tniraj7@gmail.com>
2019-09-02 09:37:20 +0900

    added and configured table view

---
+++ Note/Note/Controllers/NoteVC.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+class NoteVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+}
+
